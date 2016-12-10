7b545d92b2ebd7000928f4a0dba07a4a111f9b0f
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-12-10 16:53:43 +0200

    Hindent test, Ignore .stack-work

--- test/Main.hs
+++ test/Main.hs
@@ -11,21 +11,24 @@ pname = "haskonf-usage"
 
 main :: IO ()
 main = do
-  real <- getDataFileName "haskonf-usage.hs"
-  doesConfigExist pname >>= (flip unless) (copyReal pname real)
-  args <- getArgs
-  mainDo args
+    real <- getDataFileName "haskonf-usage.hs"
+    doesConfigExist pname >>= (flip unless) (copyReal pname real)
+    args <- getArgs
+    mainDo args
 
 mainDo :: [String] -> IO ()
-mainDo ("--rebuild" : xs) = do
-  _ <- rebuild pname -- Bad: errors should be addressed in real applications
-  launch xs
+mainDo ("--rebuild":xs) = do
+    _ <- rebuild pname
+    -- -^- Bad: errors should be addressed in real applications
+    launch xs
+
 mainDo xs = do
-  _ <- build pname   -- Bad: errors should be addressed in real applications
-  launch xs
+    _ <- build pname
+    -- -^- Bad: errors should be addressed in real applications
+    launch xs
 
 launch :: [String] -> IO ()
 launch args = do
-  dir  <- appDir  pname
-  me   <- getProgName
-  runFrom me dir (binName pname) args
+    dir <- appDir pname
+    me <- getProgName
+    runFrom me dir (binName pname) args
